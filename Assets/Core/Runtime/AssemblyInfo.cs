627895d295f3c7489d18cff0bc14e4a5c5b9430c
Nicolas Stephan <56815932+nicolas-stephan@users.noreply.github.com>
2026-04-26 12:06:13 +0200

    chore: update internals visibility and namespace configurations for test assemblies
    - Adjust `InternalsVisibleTo` definitions to reflect proper namespace alignment.
    - Update `.asmdef` and `.DotSettings` files to include specific test folders in namespace exclusions.

--- Assets/Core/Runtime/AssemblyInfo.cs
+++ Assets/Core/Runtime/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.2.0")]
 
 [assembly: InternalsVisibleTo("NS.Core.Editor")]
-[assembly: InternalsVisibleTo("Core.Tests")]
-[assembly: InternalsVisibleTo("Core.Tests.Performance.Runtime")]
-[assembly: InternalsVisibleTo("Core.Tests.Runtime")]
+[assembly: InternalsVisibleTo("NS.Core.Tests.Editor")]
+[assembly: InternalsVisibleTo("NS.Core.Tests.Performance.Runtime")]
+[assembly: InternalsVisibleTo("NS.Core.Tests.Runtime")]
